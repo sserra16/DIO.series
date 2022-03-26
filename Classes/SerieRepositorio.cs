13e42418a58ecb6f2b0d75d12e3894b61f87040b
Samuel Serra <sserradev@gmail.com>
2022-03-26 11:04:42 -0300

    First Commit

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using Dio.series.interfaces;
+
+namespace Dio.series 
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
